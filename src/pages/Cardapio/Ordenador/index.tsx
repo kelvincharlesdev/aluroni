844916b730d6d6f99a675551da9e9caaffa9f188
Kelvin Charles <kelvinc1291@hotmail.com>
2023-11-27 20:25:44 -0300

    feat: Implementa componente de Ordenador

---
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./Ordenador.module.scss"
+import opcoes from "./opcoes.json"
+
+export const Ordenador  = () => {
+    return (
+        <button className={styles.ordenador}>
+
+            <span>Ordenar Por</span>
+
+            <div className={styles.ordenador__options}>
+                {opcoes.map(opcao => (
+                    <div className={styles.ordenador__option} key={opcao.value}>
+                        {opcao.nome}
+                    </div>
+                ))}
+
+            </div>
+        </button>
+    )
+}

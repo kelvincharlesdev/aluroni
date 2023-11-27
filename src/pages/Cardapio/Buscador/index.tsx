ecef5ab42be688df055359ceeafdcf923aec353c
Kelvin Charles <kelvinc1291@hotmail.com>
2023-11-27 20:22:45 -0300

    fix: Implementa placeholder em input

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -12,6 +12,7 @@ export  const Buscador = ( {busca, setBusca} :Props) => {
             <input
              value={busca}
              onChange={(evento) => setBusca(evento.target.value)}
+            placeholder="Buscar"
             />
             <CgSearch
                 size={20}

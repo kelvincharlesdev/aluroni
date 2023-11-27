b45112d130210f1f91969f04d1c0220759e9a765
Kelvin Charles <kelvinc1291@hotmail.com>
2023-11-27 20:26:29 -0300

    fix: Implementa pequenas correções e alterações gerais

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,10 +2,12 @@ import { useState } from "react"
 import { Buscador } from "./Buscador"
 import styles from "./Cardapio.module.scss"
 import {ReactComponent as  Logo} from "assets/logo.svg" 
+import { Filtros } from "./Filtros"
+import { Ordenador } from "./Ordenador"
 
 export const Cardapio = () => {
     const [busca, setBusca] = useState("")
-
+    const [ filtro ,setFiltro] = useState<number | null>(null)
     return (
         <main>
             <nav className={styles.menu}>
@@ -24,7 +26,13 @@ export const Cardapio = () => {
                 </h3>
 
                 <Buscador busca={busca} setBusca={setBusca}/>
+                <div className={styles.cardapio__filtros}>
+                    <Filtros filtro={filtro} setFiltro={setFiltro}/>
+                    <Ordenador/>
+                </div>
+
             </section>
+
 
         </main>
     )

aad49eb1b50d80d8c66bf83c605086e1a4bfb2fb
Kelvin Charles <kelvinc1291@hotmail.com>
2023-12-05 20:36:09 -0300

    feat: Implementa funcionalidade de filtros

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,24 +1,64 @@
-import { Item } from "./Item"
-import  cardapio  from "./itens.json"
-import styles from './Itens.module.scss'
+import { useEffect, useState } from "react";
+import { Item } from "./Item";
+import cardapio from "./itens.json";
+import styles from "./Itens.module.scss";
 
-
-export const Itens = () => {
-    return (
-        <div className={styles.itens}>
-            {cardapio.map(item => (
-              <Item 
-                key={item.id}
-                title={item.title}
-                description={item.description}
-                id={item.id}
-                photo={item.photo}
-                price={item.price}
-                serving={item.serving}
-                category={item.category}
-                size={item.size}
-                />
-            ))}
-        </div>
-    )
+interface Props {
+  busca: string;
+  filtro: number | null;
+  ordenador: string;
 }
+
+export const Itens = ({ busca, filtro, ordenador }: Props) => {
+  const [lista, setLista] = useState(cardapio);
+
+  const testaBusca = (title: string) => {
+    const regex = new RegExp(busca, "i");
+    return regex.test(title);
+  };
+
+  const testaFiltro = (id: number) => {
+    if (filtro !== null) return filtro === id;
+
+    return true;
+  };
+
+  const ordenar = (novaLista: typeof cardapio) => {
+    switch (ordenador) {
+      case "porcao":
+        return novaLista.sort((a, b) => (a.size > b.size ? 1 : -1));
+      case "qtd_pessoas":
+        return novaLista.sort((a, b) => (a.serving > b.serving ? 1 : -1));
+      case "preco":
+        return novaLista.sort((a, b) => (a.price > b.price ? 1 : -1));
+      default:
+        return novaLista;
+    }
+  };
+
+  useEffect(() => {
+    const novaLista = cardapio.filter(
+      (item) => testaBusca(item.title) && testaFiltro(item.category.id)
+    );
+
+    setLista(ordenar(novaLista));
+  }, [busca, filtro, ordenador]);
+
+  return (
+    <div className={styles.itens}>
+      {lista.map((item) => (
+        <Item
+          key={item.id}
+          title={item.title}
+          description={item.description}
+          id={item.id}
+          photo={item.photo}
+          price={item.price}
+          serving={item.serving}
+          category={item.category}
+          size={item.size}
+        />
+      ))}
+    </div>
+  );
+};

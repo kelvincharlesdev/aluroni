70a7f1169334b2efaaeb7966f12bc82ffc92b4b8
Kelvin Charles <kelvinc1291@hotmail.com>
2023-12-05 20:37:57 -0300

    fix: Implementa correções gerais

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,44 +1,37 @@
-import { useState } from "react"
-import { Buscador } from "./Buscador"
-import styles from "./Cardapio.module.scss"
-import {ReactComponent as  Logo} from "assets/logo.svg" 
-import { Filtros } from "./Filtros"
-import { Ordenador } from "./Ordenador"
-import { Itens } from "./Itens"
+import { useState } from "react";
+import { Buscador } from "./Buscador";
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import { Filtros } from "./Filtros";
+import { Ordenador } from "./Ordenador";
+import { Itens } from "./Itens";
 
 export const Cardapio = () => {
-    const [busca, setBusca] = useState("")
-    const [ filtro ,setFiltro] = useState<number | null>(null)
-    const [ordenador, setOrdenador] = useState("")
+  const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
 
-    return (
-        <main>
-            <nav className={styles.menu}>
-                <Logo/>
-            </nav>
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
 
-            <header className={styles.header}>
-                <div className={styles.header__text}>
-                    A casa do código e da massa
-                </div>
-            </header>
+      <header className={styles.header}>
+        <div className={styles.header__text}>A casa do código e da massa</div>
+      </header>
 
-            <section className={styles.cardapio}>
-                <h3 className={styles.cardapio__titulo}>
-                    Cardápio
-                </h3>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
 
-                <Buscador busca={busca} setBusca={setBusca}/>
-                <div className={styles.cardapio__filtros}>
-                    <Filtros filtro={filtro} setFiltro={setFiltro}/>
-                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
-                </div>
+        <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+        </div>
 
-                <Itens/>
-
-            </section>
-
-
-        </main>
-    )
-}
+        <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+      </section>
+    </main>
+  );
+};

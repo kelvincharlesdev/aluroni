a9db6ef8045895f8b41218bc8e966c999a03f91f
Kelvin Charles <kelvinc1291@hotmail.com>
2023-12-05 19:12:58 -0300

    feat: Adiciona o componente Item ao componente Cardapio

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,6 +4,7 @@ import styles from "./Cardapio.module.scss"
 import {ReactComponent as  Logo} from "assets/logo.svg" 
 import { Filtros } from "./Filtros"
 import { Ordenador } from "./Ordenador"
+import { Itens } from "./Itens"
 
 export const Cardapio = () => {
     const [busca, setBusca] = useState("")
@@ -32,6 +33,8 @@ export const Cardapio = () => {
                     <Filtros filtro={filtro} setFiltro={setFiltro}/>
                     <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
                 </div>
+
+                <Itens/>
 
             </section>
 
